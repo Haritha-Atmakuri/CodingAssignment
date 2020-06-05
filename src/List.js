b7f2a8df8ab22674ff94fb9114d08f08f61df72e
HArrY <harry@HArrys-MBP.attlocal.net>
2020-06-04 21:24:01 -0500

    Completed all changes

--- src/List.js
+++ src/List.js
@@ -2,9 +2,6 @@ import React, { useRef, useState } from 'react';
 import './List.css';
 
 export default function List(props) {
-  const myDiv = useRef(null);
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(1);
   const list = props.myList.map((data, index) => <div key={index}>
 
     <div className="container">
@@ -17,28 +14,16 @@ export default function List(props) {
       {data.title}
     </div>
     </div>
-    {/* <div className="title">
-      {data.title}
-    </div> */}
+    
   </div>)
   return (
     <div className="mainList">
       {props.myList.length > 0 ? <h2 className="headings">{props.heading}</h2> : ''}
-      <div className="List" ref={myDiv}>
+      <div className="List">
 
         {list}
 
       </div>
-      {props.myList.length > 3 && left > 0 ? <a className="prev" onClick={() => {
-        myDiv.current.scrollLeft -= 250
-        setLeft(myDiv.current.scrollLeft);
-
-      }}>&#10094;</a> : ''}
-      {props.myList.length > 3 && left != right ? <a className="next" onClick={() => {
-        myDiv.current.scrollLeft += 250;
-        setRight(left)
-        setLeft(myDiv.current.scrollLeft);
-      }}>&#10095;</a> : ''}
     </div>
   );
 }
